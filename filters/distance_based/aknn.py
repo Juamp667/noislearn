b4939fbac2e3a6fd861a9162358a8a92c44669d7
Juamp667 <jmpadillagalera@gmail.com>
2026-07-07 15:15:11 +0200

    ordering files

--- filters/distance_based/aknn.py
+++
@@ -1,6 +0,0 @@
-"""
-AllKNN label-noise filtering.
-"""
-
-# Re-export the imbalanced-learn implementation under this namespace.
-from imblearn.under_sampling import AllKNN
--- filters/distance_based/tomeklinks.py
+++
@@ -1,9 +0,0 @@
-"""
-Tomek-links label-noise filtering.
-"""
-
-# Re-export the imbalanced-learn implementation under this namespace.
-try:
-    from imblearn.under_sampling import TomekLinks
-except ModuleNotFoundError:
-    TomekLinks = None
